b7418e2145b9907cfa0941fb5393081325029bc3
Edijs Mezits <edijs.mezits@gmail.com>
2023-11-15 17:49:14 +0200

    Added the "extra" tasks and hover effects

--- html_ts_boilerplate-master/src/scripts.ts
+++ html_ts_boilerplate-master/src/scripts.ts
@@ -1,15 +1,16 @@
-const button1 = document.querySelector('.js-button-1');
-const button2 = document.querySelector('.js-button-2');
-const button3 = document.querySelector('.js-button-3');
-const button4 = document.querySelector('.js-button-4');
-const button5 = document.querySelector('.js-button-5');
-const button6 = document.querySelector('.js-button-6');
-const button7 = document.querySelector('.js-button-7');
-const square1 = document.querySelector('js-square1');
+const buttons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.button');
+const button1: HTMLButtonElement = document.querySelector('.js-button-1');
+const button2: HTMLButtonElement = document.querySelector('.js-button-2');
+const button3: HTMLButtonElement = document.querySelector('.js-button-3');
+const button4: HTMLButtonElement = document.querySelector('.js-button-4');
+const button5: HTMLButtonElement = document.querySelector('.js-button-5');
+const button6: HTMLButtonElement = document.querySelector('.js-button-6');
+const button7: HTMLButtonElement = document.querySelector('.js-button-7');
+const square1: HTMLDivElement = document.querySelector('#js-square1');
+const square5: HTMLDivElement = document.querySelector('#js-square5');
+const input: HTMLInputElement = document.querySelector('.footer__input');
 
 button1.addEventListener('click', () => {
-  const square1 = document.getElementById('js-square1');
-
   square1.style.backgroundColor = '#E7D52D';
 });
 
@@ -35,7 +36,6 @@ button4.addEventListener('click', () => {
 
 button5.addEventListener('click', () => {
   const colorsArr = ['#A5FFD6', '#FC6471', '#7D5BA6', '#18FF6D', '#23B5D3'];
-  const square5 = document.getElementById('js-square5');
 
   square5.style.backgroundColor = colorsArr[Math.floor(Math.random() * colorsArr.length)];
 });
@@ -46,7 +46,7 @@ button6.addEventListener('click', () => {
   const square6Interval = setInterval(() => {
     square6.innerHTML = counter.toString();
     square6.style.fontSize = '56px';
-    counter++;
+    counter += 1;
 
     if (counter > 10) {
       clearInterval(square6Interval);
@@ -58,14 +58,58 @@ button7.addEventListener('click', () => {
   const allSquares = document.getElementsByClassName('square');
   const background = document.getElementById('js-background');
 
-  for (let i = 0; i < allSquares.length; i++) {
+  for (let i = 0; i < allSquares.length; i += 1) {
     const element = allSquares[i] as HTMLElement;
     element.style.backgroundColor = '#18D5E1';
   }
   background.style.backgroundColor = '#000000';
+  const text = document.getElementById('footer__header');
+  text.style.color = 'white';
 });
 
 square1.addEventListener('mouseenter', () => {
-  const squareElement = square1 as HTMLElement;
-  squareElement.style.backgroundColor = 'red';
+  square1.style.backgroundColor = 'red';
 });
+
+square1.addEventListener('mouseleave', () => {
+  square1.style.backgroundColor = '';
+});
+
+let counter = 0;
+let square5Interval: any;
+square5.addEventListener('mouseenter', () => {
+  counter = 1;
+  square5Interval = setInterval(() => {
+    square5.innerHTML = counter.toString();
+    square5.style.fontSize = '56px';
+    counter++;
+    if (counter > 10) {
+      clearInterval(square5Interval);
+    }
+  }, 1000);
+});
+
+square5.addEventListener('mouseleave', () => {
+  counter = 0;
+  square5.innerHTML = counter.toString();
+  square5.style.fontSize = '56px';
+  clearInterval(square5Interval);
+});
+
+input.onkeyup = () => {
+  document.getElementById('footer__header').innerHTML = input.value;
+};
+
+// Hover effects:
+
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+
+  button.addEventListener('mouseenter', () => {
+    button.style.opacity = '0.7';
+  });
+
+  button.addEventListener('mouseleave', () => {
+    button.style.opacity = '';
+  });
+}
